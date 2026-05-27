8cdf94f4adb94bd084b7f48f8532ccddd5ec4fe8
Mystichunterz <calvinlai_clai@protonmail.com>
2026-05-27 07:37:07 +0100

    Fix pm2 interpreter to Node v20.20.2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "Collei-Bot-v3",
         script: "index.js",
-        interpreter: "/home/pi/.nvm/versions/node/v20.11.1/bin/node",
+        interpreter: "/home/pi/.nvm/versions/node/v20.20.2/bin/node",
         env: {
             NODE_ENV: "production"
         }
